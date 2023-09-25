c89dabe0758e448b77d666f6be6c37836a19bdb1
juanbaridon <juanbaridonjd@hotmail.com>
2023-09-25 16:05:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ fetch (fetchURL)
     return response.json(); // Convierte la respuesta en formato JSON.
   })
 .then (data => {
-
+showData(data)
 })
 .catch(error => {
     console.error('Hubo un error:', error);
@@ -35,5 +35,24 @@ fetch (fetchURL)
 
 function showData(data) {
     const contenedor = document.getElementById("contenedor")
-    
+
+    for (const item of data) {
+        contenedor.innerHTML += 
+         `<div class="profile-pic"><img src="https://xsgames.co/randomusers/avatar.php?g=pixel" /></div>
+        <div class="content">
+          <div class="names">
+            <p class="full-name">${item.name}</p>
+            <p class="user-name">@${item.name}</p>
+            <p class="time"> 27mins</p>
+          </div>
+        </div>
+        <div class="tweet-content">
+          <p>${item.text}</p>
+        </div>
+        <div class="tweet-icons">
+          <i class="fa fa-comment" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+        </div>`
+    }
 }
